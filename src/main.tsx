a18a84e6d86bc08997a2659a6c9cf5d8a5776e85
Yashwanth B M <yashwanthbm36@gmail.com>
2023-12-25 11:20:30 +0530

    route setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,28 @@
-
+import { BrowserRouter} from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// import React from "react";
+// import { RouterProvider,Route,createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+
+// const router = createBrowserRouter(
+//     createRoutesFromElements(
+//         <Route path="/" element={<App/>}>
+
+//         </Route>
+//     )
+// )
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//     <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>,
+
+// )
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App/>
+<BrowserRouter>
+<App/>
+</BrowserRouter>
+
 )
